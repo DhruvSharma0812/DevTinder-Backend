80a6c13888e1f1e646ce5944638c443c487a839e
Dhruv Sharma <sharmdhruv1234@gmail.com>
2024-09-20 22:17:00 +0530

    playing with http request

--- src/app.js
+++ src/app.js
@@ -2,11 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.use ('/', (req, res) => {
-    res.send ("Hello From Dhruv");
+app.get(/.*fly$/, (req, res) => {
+    res.send("abc working fine");
+});
+
+app.get ('/user', (req, res) => {
+    console.log (req.query);
+    res.send (req.query);
 })
 
-app.listen(3000, () => {
+app.get('/user/:userid', (req, res) => {
+    console.log(req.params); // Log route parameters
+    console.log (req.query)
+    let a = req.params.userid; // Access userid from route parameters
+    res.send({ fname: "Dhruv", lname: "Sharma", userid: a });
+});
+
+
+app.use ('/test', (req, res) => {
+    res.send ("Hello From test");
+})
+
+// app.use ('/', (req, res) => {
+//     res.send ("Hello From Dhruv");
+// })
+
+app.listen(7777, () => {
     // This function takes port number and callback function
     console.log ("Server is Running on port 3000....");
 })
