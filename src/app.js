5c4a3023ee3e2c231bbe31cc1a00942964135015
Dhruv Sharma <sharmdhruv1234@gmail.com>
2024-09-22 12:29:15 +0530

    InitialiIng Database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 const connectDB = require("./config/database");
+const User = require ("./model/user")
+
+app.post ("/signup", async (req, res) => {
+    // creating new instance of user Model
+    const user = new User ({
+        firstName : "user1",
+        lastName : "test",
+        emailId : "user1@gmail.com",
+        password : "12345678",
+    })
+
+    try {
+        await user.save ();
+        res.send ("User Added Successfully...!");
+    }
+    catch (err) {
+        res.status (400). send ("Some Error While Adding A User...!!");
+    }
+})
 
 connectDB()
     .then(() => {
