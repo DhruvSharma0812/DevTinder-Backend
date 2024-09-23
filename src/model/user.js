087c513ff73e559d786c5e88137fd6555ef24348
Dhruv Sharma <sharmdhruv1234@gmail.com>
2024-09-23 21:23:57 +0530

    Schema Validation

--- src/model/user.js
+++ src/model/user.js
@@ -1,30 +1,64 @@
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema ({
-    firstName : {
-        type : String,
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 4,
+            maxLength: 20,
+        },
+
+        lastName: {
+            type: String,
+        },
+
+        emailId: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            trim: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        age: {
+            type: Number,
+            min: 18,
+        },
+
+        gender: {
+            type: String,
+            validate(value) {
+                if (!["male", "female", "other"].includes(value)) {
+                    throw new Error("Gender Data Is Not Valid");
+                }
+            },
+        },
+
+        photoUrl: {
+            type: String,
+            default: "https://geographyandyou.com/images/user-profile.png",
+        },
+
+        about: {
+            type: String,
+            default: "This is Default About",
+        },
+
+        skills: {
+            type: [String],
+        },
     },
 
-    lastName : {
-        type : String,
-    },
-
-    emailId : {
-        type : String,
-    },
-
-    password : {
-        type : String,
-    },
-
-    age : {
-        type : Number,
-    },
-
-    gender : {
-        type : String,
+    {
+        timestamps : true,
     }
-})
+);
 
-const UserModel = mongoose.model ("User", userSchema);
+const UserModel = mongoose.model("User", userSchema);
 module.exports = UserModel;
