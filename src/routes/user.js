e7e615e0cab19b7dd563d08939e97b61f131c4af
Dhruv Sharma <sharmdhruv1234@gmail.com>
2024-10-10 02:22:30 +0530

    Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../model/connectionRequest");
+const User = require("../model/user");
 const userRouter = express.Router ();
 
 const USER_SAFE_DATA = "firstName lastName age gender photoUrl about skills"
@@ -64,6 +65,49 @@ userRouter.get ("/user/connections", userAuth, async (req, res) => {
     }
 })
 
+userRouter.get ("/user/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        // Pagination
+        const page = parseInt (req.query.page) || 1;
+        let limit = parseInt (req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
 
+        // Find all connection related to user
+        const connectionRequests = await ConnectionRequest.find ({
+            $or : [
+                { fromUserId : loggedInUser._id },
+                { toUserId : loggedInUser._id },
+            ],
+        }).select ("fromUserId toUserId");
+
+        // Store in set to remove duplicates
+        const hiddenUserFromFeed = new Set ();
+        connectionRequests.forEach ( (request) => {
+            hiddenUserFromFeed.add (request.fromUserId.toString());
+            hiddenUserFromFeed.add (request.toUserId.toString());
+        });
+
+        // Find all user except the hidden ones
+        const users = await User.find ({
+            $and : [
+                {_id : {$nin : Array.from (hiddenUserFromFeed)}},
+                {_id : {$ne : loggedInUser._id} },
+            ]
+        })
+        .select (USER_SAFE_DATA)
+        .skip (skip)
+        .limit (limit)
+
+        res.json ({
+            message : "Feed Fetched Successfully...!",
+            data : users,
+        })
+    }
+    catch (err) {
+        res.status(400).send ("Error : " + err.message);
+    }
+})
 
 module.exports = userRouter;
