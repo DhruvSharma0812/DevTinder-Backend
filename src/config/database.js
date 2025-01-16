2b1a809e5eb1ecf87467b5564a31fc77dcdd29e7
Dhruv Sharma <sharmdhruv1234@gmail.com>
2025-01-16 21:54:58 +0530

    db

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,18 @@
 const mongoose = require ('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect (
-        "mongodb+srv://sharmdhruv1234:Dsapk0812@devtinder.ewdek.mongodb.net/DevTinder"
-    )
+    try {
+        await mongoose.connect (
+            "mongodb+srv://sharmdhruv1234:Dsapk0812@devtinder.ewdek.mongodb.net/"
+        )
+        console.log ("DB Connected");
+    }
+
+    catch (err) {
+        console.error("Database connection error:", err.message);
+        throw err;
+    }
 }
 
 module.exports = connectDB;
+
